89ea70b9d75b9976a1c813cb6c1eb44d424f1f6f
PrasannaLakshmiMaka <101312499+PrasannaLakshmiMaka@users.noreply.github.com>
2022-04-03 21:38:45 +0530

    Add files via upload

---
+++ 3_Implementation/getLeadingCandidate.h
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void getLeadingCandidate(){
+    printf("\n\n  #### Leading Candiate ####\n\n");
+    if(votesCount1>votesCount2 && votesCount1>votesCount3 && votesCount1 >votesCount4)
+    printf("[%s]",CANDIDATE1);
+    else if (votesCount2>votesCount3 && votesCount2>votesCount4 && votesCount2 >votesCount1)
+    printf("[%s]",CANDIDATE2);
+    else if(votesCount3>votesCount4 && votesCount3>votesCount2 && votesCount3 >votesCount1)
+    printf("[%s]",CANDIDATE3);
+    else if(votesCount4>votesCount1 && votesCount4>votesCount2 && votesCount4 >votesCount3)
+    printf("[%s]",CANDIDATE4);
+    else
+    printf("----- Warning !!! No-win situation----");    
+    
+    
+    
+}

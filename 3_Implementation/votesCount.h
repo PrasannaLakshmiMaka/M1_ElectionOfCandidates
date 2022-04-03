1065c496849c4f8f67e55f2702935be2c6f6cd12
PrasannaLakshmiMaka <101312499+PrasannaLakshmiMaka@users.noreply.github.com>
2022-04-03 21:35:15 +0530

    Add files via upload

---
+++ 3_Implementation/votesCount.h
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void votesCount(){
+printf("\n\n ##### Voting Statics ####");
+printf("\n %s - %d ", CANDIDATE1, votesCount1);
+printf("\n %s - %d ", CANDIDATE1, votesCount2);
+printf("\n %s - %d ", CANDIDATE1, votesCount3);
+printf("\n %s - %d ", CANDIDATE1, votesCount4);
+printf("\n %s - %d ", "Spoiled Votes", spoiledtvotes); 
+}
